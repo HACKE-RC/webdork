9e23ef2c117398d3d400de8adfd0c282b88732a4
Rahul rc <60568652+HACKE-RC@users.noreply.github.com>
2021-01-19 10:38:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from os import system
 try:
-        import googlesearch
+    import bs4, requests
 except:
     print("[i] Installing requirements.txt... [i]")
-    system("pip3 install -r requirements.txt")
+    system("sudo pip3 install -r requirements.txt")
 
 system("cp webdork.py /usr/bin/webdork")
 print("[i] The tool has been installed. [i]\n [i] Use the command webdork -h to Get started! [i]")
