136c9b4ce120050cbd4de7cd245cc324cd676c15
aksl337 <34566812+aksl337@users.noreply.github.com>
2021-01-19 17:31:01 +0530

    1.typo in README 2.chmod 755 in setup.py and termux-setup.py 3. added shebang line to webdork.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ except:
     print("[i] Installing requirements.txt... [i]")
     system("sudo pip3 install -r requirements.txt")
 
-system("cp webdork.py /usr/bin/webdork")
+system("cp webdork.py /usr/bin/webdork && chmod 755 /usr/bin/webdork")
 print("[i] The tool has been installed. [i]\n [i] Use the command webdork -h to Get started! [i]")
--- termux-setup.py
+++ termux-setup.py
@@ -5,5 +5,5 @@ except:
     print("[i] Installing requirements.txt... [i]")
     system("pip3 install -r requirements.txt")
 
-system("cp webdork.py /data/data/com.termux/files/usr/bin/webdork")
+system("cp webdork.py /data/data/com.termux/files/usr/bin/webdork && chmod 755 /data/data/com.termux/files/usr/bin/webdork")
 print("[i] The tool has been installed. [i]\n [i] Use the command webdork -h to Get started! [i]")
